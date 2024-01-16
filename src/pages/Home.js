284def9544eb529a8318f766dcbbeca6b98af1f7
Lagomsmart <jesperbennett@protonmail.com>
2024-01-16 14:24:41 -0500

    stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,20 @@ import React from "react";
 
 export default function Home() {
 	return (
-		<div class="flex w-full max-w-[1920px] items-center flex-col md:p-10 md:mt-8">
-			<section class="flex flex-col-reverse md:flex-row justify-center md:justify-evenly w-full">
-				<div>
-					<div></div>
+		<div class="flex w-full max-w-[1140px] items-center flex-col md:p-10 md:mt-8">
+			<section class="flex justify-center w-full">
+				<div class="flex h-[100px] w-full bg-black">
+					<div class="text-white">
+						Miami Code - Expert Web Development and Software
+						Solutions
+					</div>
 				</div>
-				<div></div>
 			</section>
-			<section></section>
+			<section class="flex flex-row w-full justify-center">
+				<div class="flex w-[30%] bg-red-100">test2</div>
+				<div class="flex w-[30%] bg-red-100">test2</div>
+				<div class="flex w-[30%] bg-red-100">test2</div>
+			</section>
 			<section></section>
 		</div>
 	);
