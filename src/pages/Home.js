ca131ef76ea0bb3ae9004e8c3e5e89d12826b054
Lagomsmart <jesperbennett@protonmail.com>
2024-01-16 13:19:55 -0500

    added wailwind and other initial docs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Home() {
-	return <div>test test test</div>;
+	return (
+		<div class="flex w-full max-w-[1920px] items-center flex-col md:p-10 md:mt-8">
+			<section class="flex flex-col-reverse md:flex-row justify-center md:justify-evenly w-full">
+				<div>
+					<div></div>
+				</div>
+				<div></div>
+			</section>
+			<section></section>
+			<section></section>
+		</div>
+	);
 }
