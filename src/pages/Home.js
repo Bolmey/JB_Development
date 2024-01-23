274e96cfecd279c90fc6bb3c4aa9760ba93897c3
Lagomsmart <jesperbennett@protonmail.com>
2024-01-23 08:56:23 -0500

    color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,15 @@ import ButtonDefault from "../components/ButtonDefault";
 
 export default function Home() {
 	return (
-		<div class="flex w-full max-w-[1140px] items-center flex-col md:p-10 md:mt-8">
-			<section class="flex justify-center w-full">
-				<div class="flex flex-col h-[400px] w-full bg-black">
+		<div class="flex w-full items-center flex-col">
+			<section class="flex  justify-center w-full bg-black md:p-10">
+				<div class="flex flex-col h-[400px] max-w-[1140px] w-full ">
 					<div class="text-white text-6xl max-w-[80%]">
 						Miami Code - Expert Web Development and Software
 						Solutions
 					</div>
-					<ButtonVariants />
-					
+					<ButtonDefault />
 				</div>
-				
 			</section>
 
 			<section class="flex w-full justify-center bg-gray-500">
