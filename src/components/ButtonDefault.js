5f7cd052206cfc365c373ffd156eb980bcabf665
Lagomsmart <jesperbennett@protonmail.com>
2024-01-23 08:35:52 -0500

    stuff

---
+++ src/components/ButtonDefault.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@material-tailwind/react";
+
+export default function ButtonDefault() {
+	return (
+		<Button className="px-5 py-2 border-2 rounded bg-white text-sm text-black">
+			Read More
+		</Button>
+	);
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ButtonDefault from "../components/ButtonDefault";
 
 export default function Home() {
 	return (
@@ -15,6 +16,21 @@ export default function Home() {
 			<section class="flex w-full justify-center bg-gray-500">
 				<div class="flex flex-wrap w-full max-w-[1140px] justify-evenly pb-5">
 					<div class="flex flex-col text-white w-[100%] md:w-[30%] bg-blue-500 mt-5 p-10 md:p-5">
+						<div class="text-2xl font-bold pt-5">
+							Web Development
+						</div>
+						<div class="pt-10 text-xl  font-bold">01</div>
+						<div class="pt-10 text-xl ">
+							We offer professional web development services using
+							the latest technologies and best practices. Our team
+							of experienced developers provides custom solutions
+							that meet your needs and exceed your expectations.
+						</div>
+						<div class="flex justify-end pt-14">
+							<ButtonDefault />
+						</div>
+					</div>
+					<div class="flex flex-col text-white w-[100%] md:w-[30%] bg-blue-500 mt-5 p-10 md:p-5">
 						<div class="text-2xl font-bold">Web Development</div>
 						<div class="pt-3 text-xl  font-bold">01</div>
 						<div class="pt-5 text-xl ">
@@ -23,40 +39,47 @@ export default function Home() {
 							of experienced developers provides custom solutions
 							that meet your needs and exceed your expectations.
 						</div>
-						<div class="pt-5">Button</div>
+						<div class="pt-10">
+							<ButtonDefault />
+						</div>
 					</div>
-					<div class="flex flex-col text-white w-[100%] md:w-[30%] bg-blue-500 mt-5 p-10">
-						<div class="text-xl font-bold">Web Development</div>
-						<div class="pt-3 text-l font-bold">01</div>
-						<div class="pt-5 text-l ">
+					<div class="flex flex-col text-white w-[100%] md:w-[30%] bg-blue-500 mt-5 p-10 md:p-5">
+						<div class="text-2xl font-bold">Web Development</div>
+						<div class="pt-3 text-xl  font-bold">01</div>
+						<div class="pt-5 text-xl ">
 							We offer professional web development services using
 							the latest technologies and best practices. Our team
 							of experienced developers provides custom solutions
-							that meet your needs and exceed your expectations.
+							that meet your needs and exceed your
+							expectations.!!!
 						</div>
-						<div class="pt-5">Button</div>
-					</div>
-					<div class="flex flex-col text-white w-[100%] md:w-[30%] bg-blue-500 mt-5 p-10">
-						<div class="text-xl font-bold">Web Development</div>
-						<div class="pt-3 text-l font-bold">01</div>
-						<div class="pt-5 text-l ">
-							We offer professional web development services using
-							the latest technologies and best practices. Our team
-							of experienced developers provides custom solutions
-							that meet your needs and exceed your expectations.
+						<div class="pt-10">
+							<ButtonDefault />
 						</div>
-						<div class="pt-5">Button</div>
 					</div>
 				</div>
 			</section>
 			<section class="flex w-full justify-center bg-gray-500">
-				<div class="flex flex-row w-full max-w-[1140px] justify-evenly pb-5">
-					<div>
-						<div>Title</div>
-						<div>Text</div>
-						<div>Button</div>
+				<div class="flex flex-row w-full max-w-[1140px] justify-evenly pb-5 text-white">
+					<div class="flex flex-col w-1/2">
+						<div>Solutions that drive your business forward</div>
+						<div>
+							Miami Code is a team of highly skilled web
+							developers and designers dedicated to delivering
+							custom web solutions and software development
+							services. We have extensive experience in developing
+							web and mobile applications tailored to your unique
+							business needs. Let us help you take your business
+							to new heights with our cutting-edge technology
+							solutions.
+						</div>
+						<div class="pt-10">
+							<ButtonDefault />
+						</div>
 					</div>
-					<div>Image</div>
+					<div class="flex w-1/2">
+						<div>Image</div>
+					</div>
 				</div>
 			</section>
 		</div>
