11702320e80cd396510fb7346a07febf7fad7a8d
Juan <juan.bolmey@gmail.com>
2024-01-25 15:18:30 -0500

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
 
 	return (
 		<nav className="bg-bgprimary w-full max-w-[1920px] border-bgprimary dark:bg-black">
-			<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<div class="max-w-[1140px] flex flex-wrap items-center justify-between mx-auto p-4">
 				<a href="/" class="flex items-center">
 					<img
 						src="https://flowbite.com/docs/images/logo.svg"
